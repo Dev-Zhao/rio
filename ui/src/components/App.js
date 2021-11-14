d2fb2d2443e4006c6c8009f8584f106b49d024bb
Yu Chen Zhao <yczhao0916@gmail.com>
2021-11-14 02:49:54 -0500

    Create chat app components

---
+++ ui/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Join from './Join';
+import Chat from "./Chat";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
