9310bdc86a59cba7424be07d393d189ffc95f65a
Yu Chen Zhao <yczhao0916@gmail.com>
2021-11-14 07:25:11 -0500

    Implement messaging between client and server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const HTTP_PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -10,20 +12,56 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
 });
 
 io.on('connection', (socket) => {
-  console.log('We have a new connection!!!');
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) return callback(error);
 
-  socket.on('join', ({ name, room }) => {
-    console.log(name, room);
+    // Send message to user who joined
+    socket.emit('message', {
+      user: 'AutoMod',
+      message: `${user.name}, welcome to the room ${room}`,
+    });
+    // Send message to other users (excluding the one that joined)
+    socket.broadcast.to(user.room).emit('message', {
+      user: 'AutoMod',
+      message: `${user.name}, welcome to the room ${room}`,
+    });
+    socket.join(user.room);
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, message });
+
+    callback();
   });
 
   socket.on('disconnect', () => {
-    console.log('User has left!!!');
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'AutoMod',
+        test: `${user.name} has left.`,
+      });
+      io.to(user.room).emit('roomData', {
+        room: user,
+        users: getUsersInRoom(user.room),
+      });
+    }
   });
 });
 
--- ui/src/components/Chat/Chat.js
+++ ui/src/components/Chat/Chat.js
@@ -3,6 +3,11 @@ import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import InfoBar from './InfoBar/InfoBar';
+import Input from './Input/Input';
+import Messages from './Messages/Messages';
+import UsersContainer from './UsersContainer/UsersContainer';
+
 import './Chat.css';
 
 let socket;
@@ -10,6 +15,9 @@ let socket;
 const Chat = () => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
   const ENDPOINT = 'localhost:5000';
 
   const search = useLocation().search;
@@ -21,15 +29,50 @@ const Chat = () => {
 
     setName(name);
     setRoom(room);
-    
-    socket.emit('join', { name, room });
+
+    socket.emit('join', { name, room }, () => {});
+
+    return () => {
+      socket.disconnect(true);
+    };
   }, [ENDPOINT, search]);
 
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  useEffect(() => {
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    });
+  }, [users]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => {
+        setMessage('');
+      });
+    }
+  };
+
   return (
-    <div>
-      Chat
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <UsersContainer users={users} />
     </div>
   );
-}
+};
 
 export default Chat;
