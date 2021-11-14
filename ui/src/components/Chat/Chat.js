4cac0dc6bcd2ce8e50d617f5f5041003c390e620
Yu Chen Zhao <yczhao0916@gmail.com>
2021-11-14 07:45:13 -0500

    Resolve socketio listeners not being cleaned up

--- ui/src/components/Chat/Chat.js
+++ ui/src/components/Chat/Chat.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
@@ -18,6 +18,7 @@ const Chat = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState();
   const ENDPOINT = 'localhost:5000';
 
   const search = useLocation().search;
@@ -30,7 +31,10 @@ const Chat = () => {
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name, room }, () => {});
+    socket.emit('join', { name, room }, (err) => {
+      setError(err);
+      console.log(err);
+    });
 
     return () => {
       socket.disconnect(true);
@@ -41,12 +45,19 @@ const Chat = () => {
     socket.on('message', (message) => {
       setMessages([...messages, message]);
     });
+    return () => {
+      socket.off('message');
+    };
   }, [messages]);
 
   useEffect(() => {
     socket.on('roomData', ({ users }) => {
       setUsers(users);
     });
+
+    return () => {
+      socket.off('roomData');
+    };
   }, [users]);
 
   const sendMessage = (e) => {
@@ -62,13 +73,26 @@ const Chat = () => {
   return (
     <div className="outerContainer">
       <div className="container">
-        <InfoBar room={room} />
-        <Messages messages={messages} name={name} />
-        <Input
-          message={message}
-          setMessage={setMessage}
-          sendMessage={sendMessage}
-        />
+        {!error ? (
+          <React.Fragment>
+            <InfoBar room={room} />
+            <Messages messages={messages} name={name} />
+            <Input
+              message={message}
+              setMessage={setMessage}
+              sendMessage={sendMessage}
+            />
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <h1>Sorry... Username is taken!</h1>
+            <Link to={`/`}>
+              <button className="button mt-20" type="submit">
+                Go Back
+              </button>
+            </Link>
+          </React.Fragment>
+        )}
       </div>
       <UsersContainer users={users} />
     </div>
