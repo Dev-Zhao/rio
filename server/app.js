07139cc44ce30805568d3e3a5b4762541319c4db
Yu Chen Zhao <yczhao0916@gmail.com>
2021-11-14 03:02:44 -0500

    Initialize socket.io for server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
+const express = require('express');
+const socketio = require('socket.io');
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const HTTP_PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(HTTP_PORT, () => {
+  console.log(`Server running on port ${HTTP_PORT}`);
 });
