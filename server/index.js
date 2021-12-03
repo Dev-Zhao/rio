2ef9b230fe6f4ce20f20e1819c038a778d5d311f
Yu Chen Zhao <yczhao0916@gmail.com>
2021-12-02 19:57:25 -0500

    Change origins

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://rio-client.herokuapp.com/',
     methods: ['GET', 'POST'],
   },
 });
--- ui/src/index.js
+++ ui/src/index.js
@@ -4,4 +4,7 @@ import SocketProvider from './contexts/SocketProvider';
 
 import App from './components/App';
 
+// Push to heroku:
+// git subtree push --prefix ui/ heroku-client master
+// git subtree push --prefix server/ heroku-server master
 ReactDOM.render(<SocketProvider><App /></SocketProvider>, document.querySelector('#root'));
